dfcb10ab4b9b3e1c8609e50636af68f6706753bb
F4K3 IDENTITY <wilsonangwz@gmail.com>
2023-06-03 19:28:37 +0800

    Added Homepage

--- TheThreeBodyGame/mainwindow.cpp
+++ TheThreeBodyGame/mainwindow.cpp
@@ -9,12 +9,19 @@ MainWindow::MainWindow(QWidget *parent) :
 {
     ui->setupUi(this);
 
+    // Set a background image
+    QString backgroundImage = "background-image: url(../Assests/edit.png);";
+    this->setStyleSheet(backgroundImage);
+
+
     QRect rect = QApplication::desktop()->availableGeometry(this);
     int width = rect.width();
     int height = rect.height();
 
-    QPixmap pixmap("../Assests/Homepage.png");
-    ui->homepage->setPixmap(pixmap);
+//    QPixmap pixmap("../Assests/edit.png");
+//    pixmap = pixmap.scaled(width + 80, height + 80, Qt::KeepAspectRatio);
+//    ui->homepage->setPixmap(pixmap);
+//    ui->homepage->resize(width, height);
 }
 
 
