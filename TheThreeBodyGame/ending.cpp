e9f2b3584aba05a08fac56a0aa65e6af0cdc7b6e
Threebodyman1 <1900010625@pku.edu.cn>
2023-07-08 15:27:49 +0800

    15.30（只差一个功能）

--- TheThreeBodyGame/ending.cpp
+++ TheThreeBodyGame/ending.cpp
@@ -1,14 +1,13 @@
 #include "ending.h"
+#include "ui_ending.h"
 #include "gamepage.h"
 #include "mainwindow.h"
-#include "ui_ending.h"
 
 ending::ending(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::ending)
 {
     ui->setupUi(this);
-    ui->label->setText("gamestatus1.");
 }
 
 ending::~ending()
@@ -19,8 +18,8 @@ ending::~ending()
 void ending::on_pushButton_clicked()
 {
     close();
-    MainWindow * mainwindow = new MainWindow();
-    mainwindow -> showMaximized();
+    MainWindow* mainwindow = new MainWindow();
+    mainwindow->showMaximized();
 }
 
 void ending::on_pushButton_2_clicked()
